3411ef010c31726853d315916c729d4c6007a47f
ahmedalbhere <ahmed01552732042@gmail.com>
2025-06-30 14:52:58 +0300

    Create User.js

---
+++ User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'cashier'],
+    default: 'cashier'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// تشفير كلمة المرور قبل الحفظ
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// مقارنة كلمة المرور المدخلة مع المشفرة
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
